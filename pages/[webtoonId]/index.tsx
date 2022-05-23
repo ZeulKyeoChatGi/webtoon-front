5fcc51b276e94c53382f0859283620f4b7959cd8
vewevteen <vewevteen0414@gmail.com>
2022-05-23 22:06:17 +0900

    1차 스타일 수정

--- pages/[webtoonId]/index.tsx
+++ pages/[webtoonId]/index.tsx
@@ -7,9 +7,22 @@ import _getWebtoonDetail from '@/api/webtoonId';
 
 const ThumbnailWrapper = styled.div`
   display: flex;
-  padding: 16px 16px 0 16px;
+  padding: 16px;
+  position: absolute;
+  width: 100%;
+  z-index: 999;
+  top: -1px;
   align-items: center;
   margin-bottom: 8px;
+  background: linear-gradient(
+    to top,
+    rgba(255, 255, 255, 0) 10%,
+    rgba(255, 255, 255, 0.5),
+    rgba(255, 255, 255, 0.7),
+    rgba(255, 255, 255, 1),
+    rgba(255, 255, 255, 1),
+    rgba(255, 255, 255, 1)
+  );
 `;
 
 const WebtoonName = styled.div`
@@ -24,11 +37,13 @@ const Thumbnail = styled.div`
   display: flex;
   justify-content: center;
   position: relative;
+  height: 250px;
 `;
 
 const TeamLabel = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: flex-end;
   gap: 6px;
   position: absolute;
   bottom: 12px;
@@ -66,7 +81,7 @@ const Score = styled.div`
 
 const ScoreDivider = styled.div`
   height: 12px;
-  border: 1px solid #c4c4c4;
+  border: 1px solid #ff6262;
   margin: 0 8px 0 8px;
 `;
 
@@ -95,13 +110,14 @@ const Label = styled.span`
   color: white;
   font-weight: 600;
   padding: 2px 4px;
-  background-color: #6e7781;
+  background-color: #2c3131;
+  font-size: 12px;
 `;
 
 const Button = styled.div`
   display: flex;
   flex: 1;
-  background-color: #31c52e;
+  background-color: #2c3131;
   justify-content: center;
   color: white;
   padding: 16px 0;
@@ -112,27 +128,38 @@ const WebtoonDetail: React.VFC = () => {
   const router = useRouter();
   const { webtoonId } = router.query;
   const webtoonData = _getWebtoonDetail(webtoonId as string);
+  const imageBgColor = webtoonData?.thumbnail_bg_color.split('#')[1];
 
-  console.log(webtoonData)
+  console.log(webtoonData);
   return webtoonData ? (
-    <div>
+    <div style={{ position: 'relative' }}>
+      <title>{webtoonData?.title}</title>
       <ThumbnailWrapper>
-        <div>
+        <div onClick={() => router.back()}>
           <Image src="/icons/ic_left_arrow.svg" alt="arrow" width={12} height={20} />
         </div>
         <WebtoonName>{webtoonData?.title}</WebtoonName>
       </ThumbnailWrapper>
-      <Thumbnail style={{ height: '250px' }}>
+      <Thumbnail>
         <img style={{ height: '100%', position: 'absolute' }} src={webtoonData?.thumbnail_first_layer} />
-        <img style={{ height: '100%', zIndex: 1 }} src={webtoonData?.thumbnail_second_layer} />
+        {webtoonData?.platform === 'NAVER' ? (
+          <div style={{ width: '100%', height: '100%', backgroundColor: `#${imageBgColor}` }} />
+        ) : (
+          <img style={{ height: '100%', zIndex: 1 }} src={webtoonData?.thumbnail_second_layer} />
+        )}
         <TeamLabel>
+          {webtoonData?.webtoon_data[0].is_completed ? (
+            <Image src="/icons/ic_complete_status.svg" alt="ic_complete_status" width={48} height={20} />
+          ) : (
+            <Image src="/icons/ic_proceed_status.svg" alt="ic_proceed_status" width={48} height={20} />
+          )}
           {webtoonData?.platform === 'NAVER' && <Image src="/icons/ic_naver.svg" alt="ic_naver" width={68} height={20} />}
           {webtoonData?.platform === 'KAKAO' && <Image src="/icons/ic_kakao.svg" alt="ic_kakao" width={68} height={20} />}
         </TeamLabel>
       </Thumbnail>
       <Info>
         <div>
-          <Image src="/icons/ic_empty_icon.svg" alt="empty" width={48} height={54} />
+          <Image src="/icons/ic_pig.svg" alt="empty" width={48} height={54} />
         </div>
         <div>
           <SaveMoneyText>
@@ -144,17 +171,30 @@ const WebtoonDetail: React.VFC = () => {
       <Divider />
       <Content>
         <Score>
-          <div>
-            <Image src="/icons/ic_star_black.svg" alt="star" width={13} height={13} />
-            {webtoonData?.webtoon_data[0].rating}
-          </div>
-          <ScoreDivider />
-          <div>
-            <Image src="/icons/ic_heart_black.svg" alt="star" width={13} height={12} />
+          {webtoonData?.webtoon_data[0].view_count && (
+            <>
+              <div style={{ display: 'flex', color: '#FF6262', gap: '4px' }}>
+                <Image src="/icons/ic_eye_red.svg" alt="eye" width={13} height={13} />
+                {webtoonData.webtoon_data[0].view_count}
+              </div>
+              <ScoreDivider />
+            </>
+          )}
+          {webtoonData?.webtoon_data[0].rating && (
+            <>
+              <div style={{ display: 'flex', color: '#FF6262', gap: '4px' }}>
+                <Image src="/icons/ic-star.svg" alt="star" width={13} height={13} />
+                {webtoonData?.webtoon_data[0].rating}
+              </div>
+              <ScoreDivider />
+            </>
+          )}
+          <div style={{ display: 'flex', color: '#FF6262', gap: '4px' }}>
+            <Image src="/icons/ic-heart.svg" alt="star" width={13} height={12} />
             {webtoonData?.webtoon_data[0].like_count}
           </div>
         </Score>
-        <Label>{webtoonData?.webtoon_data[0].is_completed ? '연재 종료' : '연재중'}</Label>
+
         <WebtoonInfo>
           <WebtoonInfoType>작품소개</WebtoonInfoType>
           <WebtoonInfoStory>{webtoonData?.description}</WebtoonInfoStory>
@@ -163,7 +203,7 @@ const WebtoonDetail: React.VFC = () => {
           <WebtoonInfoType>글/그림</WebtoonInfoType>
           <WebtoonInfoStory>
             {webtoonData?.author}
-            {webtoonData?.drawer && `/ ${webtoonData?.drawer}`}
+            {webtoonData?.drawer && ` / ${webtoonData?.drawer}`}
           </WebtoonInfoStory>
         </WebtoonInfo>
         <WebtoonInfo>
@@ -174,7 +214,7 @@ const WebtoonDetail: React.VFC = () => {
           <WebtoonInfoType>연령대</WebtoonInfoType>
           <WebtoonInfoStory>전체연령가</WebtoonInfoStory>
         </WebtoonInfo>
-        <PaytoShow>2022년 3월 27일 유료화</PaytoShow>
+        <PaytoShow>{webtoonData?.webtoon_data[0]?.paid_date} 유료화</PaytoShow>
         <Link href={webtoonData?.webtoon_url}>
           <Button>바로 정주행 하기!</Button>
         </Link>
