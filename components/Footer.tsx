116f20bfce8999392ea8c8759384bdda5061fc73
Linternas <kmg20000215@gmail.com>
2022-06-21 03:45:39 +0900

    디버깅 로그 추가

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -41,7 +41,9 @@ const Footer = () => {
   };
 
   const handleShareKakao = () => {
-    if (document.querySelector('.b-test') === null) {
+    console.log(document.querySelector('b-test'));
+    console.log(document.querySelector('.b-test'));
+    if (document.querySelector('b-test') === null) {
       shareToKakao(
         '내일이면 유료화되는 웹툰이 궁금하다면?',
         '#오늘의웹툰 #웹툰정주행 #오늘까지_무료',
