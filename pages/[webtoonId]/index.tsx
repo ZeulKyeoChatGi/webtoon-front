2851c65ca1db237ff3f26bbb3e535b1574c1ea54
linternas <kmg20000215@gmail.com>
2022-06-29 20:43:52 +0900

    카카오 중복 이미지 수정

--- pages/[webtoonId]/index.tsx
+++ pages/[webtoonId]/index.tsx
@@ -250,15 +250,14 @@ const WebtoonDetail: React.VFC = () => {
         </div>
       </ThumbnailWrapper>
       <Thumbnail>
-      <img style={{ height: '100%', position: 'absolute', zIndex: 1 }} src={webtoonData?.thumbnail_first_layer} />
+        <img style={{ height: '100%', position: 'absolute', zIndex: 1 }} src={webtoonData?.thumbnail_first_layer} />
         <img
-          style={{ height: '100%', position: 'absolute', zIndex: 2, marginLeft: webtoonData?.platform === 'NAVER' ?  webtoonData?.diffWidth : '0' }}
+          style={{ height: '100%', position: 'absolute', zIndex: 2, marginLeft: webtoonData?.platform === 'NAVER' ? webtoonData?.diffWidth : '0' }}
           src={webtoonData?.thumbnail_second_layer}
         />
 
-        {webtoonData?.platform === 'NAVER' && (
-          <div style={{ width: '100%', height: '100%', backgroundColor: `#${imageBgColor}` }} />
-        )}
+        {webtoonData?.platform === 'NAVER' && <div style={{ width: '100%', height: '100%', backgroundColor: `#${imageBgColor}` }} />}
+
         <TeamLabel>
           {webtoonData?.webtoon_data[0].is_completed ? (
             <img src="/icons/ic_complete_status.svg" alt="ic_complete_status" width={48} height={20} />
